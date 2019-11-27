aece71e967f40ba520910ec6a4822e40cf9d4d59
Ryunosuke O'Neil <roneil@fnal.gov>
2019-11-27 18:45:58 +0000

    Added Mille binary file writer, a wrapper, and a module. Probably no need to put Mille.cc/Mille.h on CVMFS. Added new plane alignment module (as a starting point), and a MilleWrapper object designed to interface between Mu2e code and Millepede.

---
+++ CosmicAlignment/src/MilleWrapper.cc
@@ -0,0 +1,43 @@
+// Ryunosuke O'Neil, 2019
+// Millepede-II Mille routine wrapper
+
+
+#include "CosmicAlignment/inc/MilleWrapper.hh"
+
+
+/**
+ * @brief Construct a new Mille Wrapper object.
+ *
+ * @param output_file the filename for the millepede binary configuration file.
+ */
+MilleWrapper::MilleWrapper(std::string output_file)
+{
+    millepede = std::make_unique<Mille>(output_file.c_str());
+}
+
+/**
+ * @brief This method facilitates, in Millepede II terms,
+ * the definition of the 'global parameters' i.e. free parameters
+ * of the alignment problem. i.e. the translation vector and rotation
+ * angles of a plane object.
+ *
+ *
+ */
+void MilleWrapper::RegisterAlignableObject(AlignableObject const& object)
+{
+    //millepede->mille()
+}
+
+/**
+ * @brief Add a new track to the Mille buffer.
+ *
+ */
+void MilleWrapper::RegisterTrack()
+{
+
+}
+
+void MilleWrapper::Save()
+{
+    millepede->end();
+}
