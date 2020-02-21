8b16c807a6d643db59356b5ce6bbe6dac5453540
Ryunosuke O'Neil <roneil@fnal.gov>
2020-02-20 19:48:28 -0600

    Code formatting patch on 35a8b79f376948cc6a8ead10c4241b8f7fc8c28f

--- ConditionsBase/inc/TrackerCalibrationStructs.hh
+++ ConditionsBase/inc/TrackerCalibrationStructs.hh
@@ -14,39 +14,38 @@
 
 namespace mu2e
 {
-  class SimpleConfig;
-  struct StrawHit;
-  class Straw;
+class SimpleConfig;
+struct StrawHit;
+class Straw;
 // simple struct to hold output of timeToDistance function
-  struct T2D {
+struct T2D {
     double _rdrift;
     double _rdrifterr;
     double _vdrift; // local drift velocity at this radius, ie dr/dt(rdrift).
     T2D() : _rdrift(0.0), _rdrifterr(1.0), _vdrift(0.0) {}
-  };
+};
 
 // simple struct to hold output of distanceToTime function
-  struct D2T {
+struct D2T {
     double _tdrift;
     double _tdrifterr;
     double _vdrift; // local drift velocity at this radius, ie dr/dt(rdrift).
-    D2T() : _tdrift(0.0),_tdrifterr(1.0), _vdrift(0.0) {}
-  };
+    D2T() : _tdrift(0.0), _tdrifterr(1.0), _vdrift(0.0) {}
+};
 
 // simple struct to hold output of energyToAmplitude function
-  struct E2A {
+struct E2A {
     double _ampl;
     double _amplerr;
     E2A() : _ampl(0.0), _amplerr(1.0) {}
-  };
+};
 
 // simple struct to hold output of amplitudeToEnergy function
-  struct A2E {
+struct A2E {
     double _edep;
     double _edeperr;
-    A2E() : _edep(0.0),_edeperr(1.0) {}
-  };
-
+    A2E() : _edep(0.0), _edeperr(1.0) {}
+};
 }
 
 #endif
